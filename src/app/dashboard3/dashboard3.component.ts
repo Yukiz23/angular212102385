7e10424ef3b4ad2979c0ef51ca876c922b5a8dd7
Yukiz23 <144302962+Yukiz23@users.noreply.github.com>
2023-10-23 11:55:44 +0700

    Add files via upload

---
+++ src/app/dashboard3/dashboard3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard3',
+  templateUrl: './dashboard3.component.html',
+  styleUrls: ['./dashboard3.component.css']
+})
+export class Dashboard3Component {
+
+}
