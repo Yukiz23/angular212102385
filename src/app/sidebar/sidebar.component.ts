fecd1c77b69827d84bdf448ea3affb67f231cc21
Yukiz23 <humantwo631@gmail.com>
2023-10-23 11:30:31 +0700

    Pertemuan 7
    Add Dashboard 2 and 3, Also Update Dashboard 1

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  @Input() moduleName: string = '';
 }
